28f1ebf3672356d57428204b66d53f0c24201d57
Sitendra Nagesh <161935812+sitendranagesh@users.noreply.github.com>
2025-06-10 19:41:08 +0530

    Correct the isntall keyword

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-sudo apt-get isntall cowsay -y
+set -e
+sudo apt-get install cowsay -y
 cowsay -f dragon "Run away, I am a dragon .... RAWS" > artwork.txt
 grep -i dragon "artwork.txt"
 cat artwork.txt
